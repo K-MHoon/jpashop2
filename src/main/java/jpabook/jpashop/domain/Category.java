084de8b676e23c1222379eeb7cb86d4011c023c0
K-MHoon <azmria117@gmail.com>
2021-10-19 23:28:59 +0900

    feat : Category Relation ManyToMany

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    // 필드 추가가 불가능하기 때문에 거의 쓰지 않는다.
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
