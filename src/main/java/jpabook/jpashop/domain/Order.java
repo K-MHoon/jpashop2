b99c1991b16a2d25540c60733d3fcc0d9374b1c9
K-MHoon <azmria117@gmail.com>
2021-10-21 23:05:06 +0900

    fix : @~One relation setting to LAZY, cascade setting, relation set Method
    - @~one relation's default setting = EAGER
    - fix this setting to FetchType.LAZY
    - cascade.ALL Setting
    - 1:N , N:1 relation setter Method create

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,14 +19,14 @@ public class Order {
     private Long id;
 
     // 연관관계의 주인
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,4 +34,22 @@ public class Order {
 
     @Enumerated(value = EnumType.STRING)
     private OrderStatus status;
+
+    // 연관관계 메서드
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+
 }
