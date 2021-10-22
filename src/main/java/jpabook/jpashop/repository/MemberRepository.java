cd9e7cec48581b7b9c8465ce8199a4281685dc07
K-MHoon <azmria117@gmail.com>
2021-10-22 23:29:52 +0900

    feat : Member Repository

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+import java.util.List;
+
+// 컴포넌트 스캔의 대상 Repository
+@Repository
+public class MemberRepository {
+
+    // 스프링이 엔티티 매니저를 주입해줌.
+    @PersistenceContext
+    private EntityManager em;
+
+    // 엔티티 매니저 팩토리를 주입받고 싶다면 주석 해제
+//    @PersistenceUnit
+//    private EntityManagerFactory emf;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
