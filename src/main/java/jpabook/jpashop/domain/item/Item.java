1f5209bc32576a5893fff2b52adca9d1a8dadf2c
K-MHoon <azmria117@gmail.com>
2021-10-25 23:15:12 +0900

    feat : Item Service, Repository
    - Create Item Service, Repository Files and Functions

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // == 비즈니스 로직 == //
+
+    /**
+     * 재고 수량을 증가한다.
+     * @param quantity 증가할 양
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고 수량을 감소한다.
+     * @param quantity 감소시킬 양
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) throw new NotEnoughStockException("need more stock");
+        this.stockQuantity = restStock;
+    }
 }
