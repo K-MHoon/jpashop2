adb9919542c95833432ab407089272fc0f1e0e32
K-MHoon <azmria117@gmail.com>
2021-11-06 23:24:50 +0900

    fix : Fetch Join
    - V3 : Order, Member, Delivery Entity using fetch join
    - V4 : Direct DTO Fetch Join

---
+++ src/main/java/jpabook/jpashop/repository/order/simplequery/OrderSimpleQueryRepository.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.repository.order.simplequery;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderSimpleQueryRepository {
+
+    private final EntityManager em;
+
+    /**
+     * select 필요한 것만 가져오지만, 재사용성 부분에서 떨어진다.
+     * 특정 화면에만 성능 최적화가 좋다.
+     */
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+        return em.createQuery("select new jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
+
+}
