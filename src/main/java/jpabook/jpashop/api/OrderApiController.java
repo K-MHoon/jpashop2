2766dd91022eeccf5686a271aae85667c4d59fd1
K-MHoon <azmria117@gmail.com>
2021-11-07 11:24:09 +0900

    feat : Create OrderAPI, OneToMany Fetch Join
    - Create OrderApiController
    - V1 : Direct Select Order Entity (need @JsonIgnore + Hibernate 5 Setting)
    - V2 : Entity To Dto (but, have Inner Entity)
    - V3 : Every Entity to Dto (outer, inner all) and using JPQL distinct

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,96 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.*;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * 엔티티 직접 노출
+     * @return
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(OrderDto::new)
+                .collect(toList());
+        return collect;
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> collect = orders.stream()
+                .map(OrderDto::new)
+                .collect(toList());
+        return collect;
+    }
+
+    @Getter
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        // 엔티티 래핑하는 것도 안된다.
+        // 엔티티에 대한 의존을 완전히 끊어야 된다.
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order o) {
+            this.orderId = o.getId();
+            this.name = o.getMember().getName();
+            this.orderDate = o.getOrderDate();
+            this.orderStatus = o.getStatus();
+            this.address = o.getDelivery().getAddress();
+            this.orderItems = o.getOrderItems().stream()
+                    .map(OrderItemDto::new)
+                    .collect(toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName; //상품 명
+        private int orderPrice; //주문 가격
+        private int count; //주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getOrderPrice();
+            this.count = orderItem.getCount();
+        }
+    }
+}
